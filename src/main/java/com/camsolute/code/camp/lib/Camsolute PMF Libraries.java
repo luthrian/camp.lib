96a9bcbeacbe690beb0caa17801ca5c3cbe5b2bb
Christopher Campbell <luthrian@gmail.com>
2018-01-15 08:12:38 +0100

    initial commit

---
+++ src/main/java/com/camsolute/code/camp/lib/Camsolute PMF Libraries.java
@@ -0,0 +1,9 @@
+package com.camsolute.code.camp.lib;
+
+public class Camsolute PMF Libraries 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/camsolute/code/camp/lib/Camsolute PMF LibrariesTest.java
@@ -0,0 +1,38 @@
+package com.camsolute.code.camp.lib;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class Camsolute PMF LibrariesTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public Camsolute PMF LibrariesTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( Camsolute PMF LibrariesTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testCamsolute PMF Libraries()
+    {
+        assertTrue( true );
+    }
+}
